aa8cbd87b5cddd420c0de00b65f02efbbf9847e2
balglave <57452072+balglave@users.noreply.github.com>
2023-09-15 10:30:23 +0200

    cov_list

--- r/config.R
+++ r/config.R
@@ -15,5 +15,5 @@ n_EOF <- c(6,2,2,1) # number of EOF to look at for each species
 copernicus.var <- c("so","bottomT","chl","thetao","o2") # "so": salinity, "chl": chlorophyll A, "thetao": SST
 # c("so","bottomT","thetao","fe","nppv","nh4","si","phyc","po4","no3","zeu","chl","o2")
 
-extract_covariates <- F # Extract covariates
+extract_covariates <- T # Extract covariates
 cov_file <- "C:/Users/test/Desktop/phd_projects/phd_full_codes/data/raw/Envir.Data/CopernicusData/" # used only if extract_covariate == T, need to have covariates on the computer
